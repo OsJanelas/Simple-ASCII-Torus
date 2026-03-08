ceeb0ceb0060060fa4be0d6ff950d4d380f38260
OsJanelas <osjanelas@gmail.com>
2026-03-07 21:49:45 -0300

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+//THIS IS A SIMPLE F# PROGRAM TO PRINT A TORUS IN ASCII ART
+//IT USES THE printfn FUNCTION TO PRINT EACH LINE OF THE TORUS
+
+Console.ForegroundColor <- ConsoleColor.Yellow
+Console.BackgroundColor <- ConsoleColor.DarkBlue
+
+printfn "           ###########           "
+printfn "     #######################     "
+printfn "  #############################  "
+printfn "############         ############"
+printfn "##########             ##########"
+printfn "########                 ########"
+printfn "########                 ########"
+printfn "########                 ########"
+printfn "########                 ########"
+printfn "##########             ##########"
+printfn "############         ############"
+printfn "  #############################  "
+printfn "     #######################     "
+printfn "           ###########           "
+printfn "Welcome to the Torus!  **********"
